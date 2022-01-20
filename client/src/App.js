93c1f4ba174032d4f5742e27def146c1e4791650
tamarcy <cohen.tamar@gmail.com>
2022-01-20 20:31:31 +0200

    with mordi's

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import "./App.css";
+import myApi from "./api/Api"
 
 function App() {
+
+  const getReq = async () => {
+    const {data} = await myApi.get("/users");
+    console.log(data);
+  }
+
   return (
     <div className="App">
+      {' '}
       <div> Welcome to the bank! </div>
-      <button>click</button>
+      <button onClick={getReq}>get</button>
     </div>
   );
 }
