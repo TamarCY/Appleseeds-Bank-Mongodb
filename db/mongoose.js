cb19c6bed74210fc92e7839f5e716f910b26b2ee
tamarcy <cohen.tamar@gmail.com>
2022-01-21 20:42:53 +0200

    added console log to mongoose

--- db/mongoose.js
+++ db/mongoose.js
@@ -6,5 +6,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const mongo_uri = process.env.MONGO_URI
 
+console.log("print: "+ mongo_uri);
+
 mongoose.connect(mongo_uri);
 
