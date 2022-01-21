c806471b8dc6911b2d2cf9b86962af41b5d9e08c
tamarcy <cohen.tamar@gmail.com>
2022-01-21 17:04:04 +0200

    button not working add heroku url to Api.js

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let myUrl = 'http://localhost:5000/api/'; //development
 
 if (process.env.NODE_ENV === 'production') {
-  myUrl = 'api';
+  myUrl = 'https://bank-mongodb.herokuapp.com/api';
 }
 export default axios.create({
   baseURL: myUrl,
