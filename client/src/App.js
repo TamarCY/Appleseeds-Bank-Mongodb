e83ba89df1db316b0bed06d6091026642f5659d4
tamarcy <cohen.tamar@gmail.com>
2022-01-22 10:54:33 +0200

    users renderd to screen onclick in development

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import myApi from './api/Api';
 
-function App() {
-  // const [user, setUser] = useState('');
+const  App = () => {
+  const [users, setUsers] = useState([{id:8}]);
 
-  console.log(process.env.NODE_ENV);
-
+  
   const getReq = async () => {
     const { data } = await myApi.get('/users');
+    setUsers(data)
     console.log(data);
   };
 
@@ -16,15 +16,41 @@ function App() {
     console.log(data);
   };
 
+
+  const renderUsers = () => {
+    const result = users.map((element)=> {
+      return (
+        <div>
+          <h3>
+            id: {element._id}
+          </h3>
+          <div>
+            credit: {element.credit}
+          </div>
+          <div>
+            cash: {element.cash}
+          </div>
+          <button>deposit</button>
+          <button>withdraw</button>
+          <button>transfer</button>
+          <button>update credit</button>
+          <button>delete</button>
+        </div>
+      )
+    })
+    return result
+  }
   return (
     <div className='App'>
-      {' '}
+      {/* {' '} */}
       Hello Bank!
       <button onClick={getReq}>get</button>
       <button onClick={postReq}>post</button>
+      {renderUsers()}
 
     </div>
   );
+
 }
 
 export default App;
